8ebfd6139ede1d7014f0ad20d4d48753e19286fa
Shouran Mu <shouran.mu@gmail.com>
2020-09-01 09:16:24 +0200

    no message

--- AngleConversion.m
+++
@@ -1,7 +0,0 @@
-%limite input theta within (-180,180]
-function theta = AngleConversion(theta)
-    while theta <= -180 || theta > 180
-       multiple = floor(theta/180);
-       theta = theta - (multiple)*180;
-    end
-end
